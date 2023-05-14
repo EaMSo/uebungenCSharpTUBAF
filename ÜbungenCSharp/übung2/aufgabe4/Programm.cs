4fe6e67066872f1e744311e4b894e6ae6b0c0d03
Emil Sommer <78460073+EaMSo@users.noreply.github.com>
2023-05-14 18:44:21 +0200

    Add files via upload

---
+++ ÜbungenCSharp/übung2/aufgabe4/Programm.cs
@@ -0,0 +1,12 @@
+using System;
+static class Program
+{
+static void Main(){
+        Smartphone Handy = new Smartphone();
+        Handy.PinÄndern();
+        Handy.PinÄndern();
+        Handy.PinÄndern();
+        Handy.PinAusgeben();
+    }
+
+}
