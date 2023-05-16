66e5e787bc05fc48b0cc11534e96e0a958610f7e
EaMSo <emil@drsommerkrenz.de>
2023-05-16 14:21:17 +0200

    änderungen in aufgabe 4 add von get set props

--- ÜbungenCSharp/übung2/aufgabe3/Program.cs
+++ ÜbungenCSharp/übung2/aufgabe3/Program.cs
@@ -1,30 +1,55 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-
 public struct Size
 {
+    private double width;
+    private double height;
+
     public Size(double width, double height)
     {
         if(width < 0 || height < 0)
         {
-            throw new System.ArgumentException(); //??????
+            throw new System.ArgumentException();
         }
         else
         {
-            this.Width = width;
-            this.Height = height;
+            this.width = width;
+            this.height = height;
         }
     }
 
-    public double Width;
-    public double Height;
+    public double Width
+    {
+        get { return width; }
+         set
+        {
+            if (value > 0)
+            {
+                width = value;
+            }
+            else throw new System.ArgumentException();
+        }
+    }
+    public double Height
+    {
+        get { return height; }
+         set
+        {
+            if (value > 0)
+            {
+                height = value;
+            }
+            else throw new System.ArgumentException(); 
+        }
+    }
 }
+
 public class Programm
 {
     static void Main()
     {
-        Size rechteck = new Size(-10.2, 32.13);
+        Size rechteck = new Size(10.2, 32.13);
+        System.Console.WriteLine(rechteck.Width);
+        rechteck.Width = 12;
         System.Console.WriteLine(rechteck.Width);
     }
 }
